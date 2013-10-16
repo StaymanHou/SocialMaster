08b2d32f4d83895e9a47270c28723ebe09a0acb4
staymanHou <hhyycc0418@gmail.com>
2013-10-16 14:55:46 -0400

    initial WebAdmin

---
+++ SocialManager/WebAdmin/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebAdmin::Application.config.secret_key_base = 'd667b354a3238b05c8464daad0d22883c71d55485b2e4da9eb144cb485289af6696f276f746963cb159bb3b436b4d02caf0b855f0e5100b84b49ef50dad39291'
