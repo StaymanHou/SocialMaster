3af747eba720d7f3bc6ca85defb2ac7b0e34ba5e
staymanHou <hhyycc0418@gmail.com>
2013-10-22 13:43:07 -0400

    start over

--- SocialManager/WebAdmin/db/migrate/20131021141236_create_site_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreateSiteCategories < ActiveRecord::Migration
-  def change
-    create_table :site_categories do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- SocialManager/WebAdmin/db/migrate/20131021142628_create_auto_modes.rb
+++
@@ -1,9 +0,0 @@
-class CreateAutoModes < ActiveRecord::Migration
-  def change
-    create_table :auto_modes do |t|
-      t.string :title
-
-      t.timestamps
-    end
-  end
-end
