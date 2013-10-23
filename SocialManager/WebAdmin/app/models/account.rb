9dee9fa4d9ca5759a3e1b2f50da8691c56a6e23c
staymanHou <hhyycc0418@gmail.com>
2013-10-23 10:22:28 -0400

    rebuild models/account

--- SocialManager/WebAdmin/app/models/account.rb
+++ SocialManager/WebAdmin/app/models/account.rb
@@ -1,3 +1,9 @@
 class Account < ActiveRecord::Base
-  has_many :acc_settings
+	has_many :acc_settings
+	before_save :default_values
+	def default_values
+		# self.deleted ||= false
+		self.deleted = false if self.deleted.nil?
+		true
+	end
 end
