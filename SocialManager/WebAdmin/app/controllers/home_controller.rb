8990cdeac9e1e233854bc6fefc32011ebd8cf653
staymanHou <hhyycc0418@gmail.com>
2013-10-24 17:39:17 -0400

    embeded third line of home index

--- SocialManager/WebAdmin/app/controllers/home_controller.rb
+++ SocialManager/WebAdmin/app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
 	def index
 		@active_page = "Home"
+		@smodules = Smodule.all
+		@smodules ||= []
+		@sites = Site.all
+		@sites ||= []
+		@tag_count = Tag.count
+		@active_accounts = Account.where(active: true)
+		@active_accounts ||= []
+		@accounts = Account.all
+		@accounts ||= []
 	end
 end
