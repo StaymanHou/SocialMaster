4cf90b0242a6fef0d21b96df88829421a49bc0d3
staymanHou <hhyycc0418@gmail.com>
2013-12-03 15:36:41 -0500

    dont track mainconfig

--- SocialManager/Lib/MainConf.py
+++
@@ -1,6 +0,0 @@
-MainConf = {
-    'IMAGE_FILE_DIR': '/home/stayman/Work/33Social/SocialMaster/src/SocialManager/WebAdmin/public/images/postimg/rss/', # please end with "/"
-    'LOAD_ITERATION': 1,
-    'PULLER_ITERATION': 300,
-    'POSTER_ITERATION': 30,
-}
