f94fc0131d1edd386505fe5e6b329ce0f86ce831
staymanHou <hhyycc0418@gmail.com>
2013-12-17 16:40:28 -0500

    fix_bug: crash when running under windows and timezone detected from rss

--- SocialManager/Lib/systemHelper.py
+++ SocialManager/Lib/systemHelper.py
@@ -1,6 +1,7 @@
 import platform
 from dateutil import parser as dateutilparser
 from datetime import datetime
+import pytz
 
 def specialize_path(gene_path):
 	if platform.system() is 'Windows':
@@ -9,6 +10,10 @@ def specialize_path(gene_path):
 
 def parseTime(t):
 	if platform.system() is 'Windows':
-		return dateutilparser.parse(t)
+		d = dateutilparser.parse(t)
+		if d.tzinfo:
+			est = pytz.timezone('US/Eastern')
+			return d.astimezone(est).replace(tzinfo=None)
+		return d
 	return datetime.strptime(t, '%a, %d %b %Y %H:%M:%S %Z')
 
